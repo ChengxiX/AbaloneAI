fba2eb04b9480cdb7f86931605f0fdfc6f3caa1e
ChengxiX <Hanson.xcx@outlook.com>
2023-03-06 15:30:22 +0800

    muleizhang

--- policy_value_net.py
+++ policy_value_net.py
@@ -15,7 +15,12 @@ from torch.autograd import Variable as V
 
 print("cuda availability = {}".format(torch.cuda.is_available()))
 print("torch.__version__ = {}".format(torch.__version__))
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+else:
+    DEVICE = torch.device("cpu")
 
+    
 '''self.board = 
 [[3, 3, 3, 3, 3, 0, 0, 0, 0],
 [3, 3, 3, 3, 3, 3, 0, 0, 0],
@@ -46,6 +51,8 @@ class op_version(tuple):
 
 '''
 
+MAP_TABLE = [36,28,20,12,4,-4,-12,-20,-28,45,37,29,21,13,5,-3,-11,-19,54,46,38,30,22,14,6,-2,-10,63,55,47,39,31,23,15,7,-1,72,64,56,48,40,32,24,16,8,81,73,65,57,49,41,33,25,17,90,82,74,66,58,50,42,34,26,99,91,83,75,67,59,51,43,35,108,100,92,84,76,68,60,52,44]
+
 def convert_board(board, player):  # player:machine's side 0:white 1:black
     """
     input: board --> list
@@ -72,17 +79,26 @@ class PolicyValueModule(nn.Module):
         # num of all action(include a lot impossible ones) =
         # = first*second*third*direction
         # = (9*9)*(9*9+1)*(9*9+1)*6 = 3267864
-        self.op = nn.Conv2d(128, 4, kernel_size=(1,))
-        self.op_xydirec = nn.Conv3d(4, 6, kernel_size=(1,))
-        self.
-        self.op_num = nn.Linear(6 * 9 * 9, 3 * 6 * 9 * 9)
-        self.op_final = nn.Linear(3 * 6 * 9 * 9,6 * 3 * 6 * 9 * 9)
+        self.op = nn.Conv2d(128, 16, kernel_size=(1,))
+        self.op_xydirec = nn.Conv2d(16, 6, kernel_size=(1,))
+        self.op_num = nn.Linear(9 * 9, 3 * 9 * 9)
+        self.op_final = nn.Conv3d(4, 6, kernel_size=(1,))
+        self.op_final = nn.Linear(3 * 9 * 9,6 * 3 * 9 * 9)
 
         # state value layers
         self.sv_conv1 = nn.Conv2d(128, 4, kernel_size=(1,))
         self.sv_fc1 = nn.Linear(4 * 9 * 9, 64)
         self.sv_fc2 = nn.Linear(64, 1)
         pass
+
+    @classmethod
+    def transform(cls, board):
+        new = [0]*81
+        for i in range(81):
+            pos = MAP_TABLE[i]
+            if pos >=0:
+                new[pos] = board[i]
+        return new
 
     def forward(self, input_board):
         # input_board --> torch.Size([4, 9, 9])
@@ -93,11 +109,23 @@ class PolicyValueModule(nn.Module):
         # action policy
         # pick x & y
         op = F.relu(self.op(x))
-        op = F.relu(self.xy(op))
+        op_xydirec = F.relu(self.op_xydirec(op))
+        # 以上op的形状是6*9*9, 第n层是指方向n，下面需要把所有的方向都变为方向0
+        list_board = op_xydirec.view(-1, 6 * 9 * 9).numpy().tolist()
+        op_final = torch.zeros([6,9,9],device=DEVICE)
+        
+        for i in range(6):
+            list_board_i = list_board[i*81:82+i*81]
+            for j in range(i):
+                list_board_i = self.transform(list_board_i)
+            list_board_i_num = F.relu(self.op_num(torch.tensor(list_board_i)))
+            list_board_i_direc = F.relu(self.op_direc(list_board_i_num))
+            op_final[i] = F.tanh(list_board_i_direc)
+            
+        
         op = F.relu(self.op_num(op))
-        op_final
         direc_plus = F.log_softmax(self.direc_plus(op))
-
+        self.transform()
 
         # state value
         sv = F.relu(self.sv_conv1(x))
