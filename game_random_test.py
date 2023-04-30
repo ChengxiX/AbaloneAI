f9211d7343980d1bdbb80a989e6131dd89a7b493
ChengxiX <Hanson.xcx@outlook.com>
2023-04-30 22:37:16 +0800

    更新测试

--- game_random_test.py
+++ game_random_test.py
@@ -3,13 +3,21 @@ import random
 
 
 if __name__ == '__main__':
-    random.seed(5)
+    random.seed(1)
     g = game.Game()
-    # g.display()
+    # g.board = [[3, 3, 3, 1, 1, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1, 0, 0, 0], [1, 1, 1, 1, 1, 1, 1, 0, 0],
+    #            [1, 1, 1, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 1, 1, 1, 1, 1, 1, 1, 1],
+    #            [0, 0, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 1, 1, 1, 1, 1]]
+    g.display()
     for i in range(200):
         choice = random.choice(g.available_op(2))
-        # print(g.convert_long_to_short(choice))
-        # print(choice)
+        print(g.convert_long_to_short(choice))
+        print(choice)
         g.operate(player=2, op=choice)
-        # g.display()
-    print('done')
+        g.display()
+
+        choice = random.choice(g.available_op(3, test=True))
+        print(g.convert_long_to_short(choice))
+        print(choice)
+        g.operate(player=3, op=choice)
+        g.display()
